0a990d2bfae4fc9be21be8efeeab39f001f3a280
Jason Sherrick <jasonsherrick@gmail.com>
2026-07-11 12:10:07 -0500

    Removed state flag from emulator

--- include/chip8.h
+++ include/chip8.h
@@ -8,15 +8,8 @@
 #include "chip8_keyboard.h"
 #include "chip8_stack.h"
 
-typedef enum {
-    CHIP8_STATE_RUNNING,
-    CHIP8_STATE_PAUSED,
-    CHIP8_STATE_PANIC
-} chip8_state_t;
-
 typedef struct chip8
 {
-    chip8_state_t state;
     chip8_memory_t memory;
     chip8_stack_t stack;
     chip8_registers_t registers;
