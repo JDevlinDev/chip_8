e22bcd7d03137ff856c74add73c223f2c535fcaf
Jason Sherrick <jasonsherrick@gmail.com>
2026-07-11 11:44:29 -0500

    added missing break statements in main.c.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+
+#include <stdio.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
 #include "chip8.h"
+#include "chip8_keyboard.h"
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+static chip8_keyboard_t keyboard;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
@@ -36,9 +40,23 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
-    if (event->type == SDL_EVENT_QUIT) {
-        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+    switch (event->type) {
+    case SDL_EVENT_QUIT:
+        return SDL_APP_SUCCESS;
+    case SDL_EVENT_KEY_DOWN:
+        int keyPressed = chip8_keyboard_map(event->key.key);
+        chip8_keyboard_down(&keyboard, keyPressed);
+        if (chip8_keyboard_is_down(&keyboard, keyPressed))
+            printf("%d was pressed\n", keyPressed);
+        break;
+    case SDL_EVENT_KEY_UP:
+        int keyReleased = chip8_keyboard_map(event->key.key);
+        chip8_keyboard_up(&keyboard, keyReleased);
+        if(!chip8_keyboard_is_down(&keyboard, keyReleased))
+            printf("%d was released\n", keyReleased);
+        break;
     }
+    
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
