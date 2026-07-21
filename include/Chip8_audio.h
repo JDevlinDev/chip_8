8979a5c26ae62510e49732650ebb4a5819bae3d4
Jason Sherrick <jasonsherrick@gmail.com>
2026-07-21 16:30:31 -0500

    Move audio struct def from src file to header

--- include/Chip8_audio.h
+++ include/Chip8_audio.h
@@ -7,7 +7,14 @@
 #define CHIP8_AUDIO_FREQ            440
 #define CHIP8_AUDIO_PERIOD          CHIP8_AUDIO_SAMPLE_RATE / CHIP8_AUDIO_FREQ
 
-typedef struct Chip8_Audio Chip8_Audio;
+typedef struct Chip8_Audio {
+   
+   SDL_AudioStream *stream;
+
+   bool is_playing;
+
+   uint8_t buffer[CHIP8_AUDIO_BUFFER_SIZE];
+} Chip8_Audio;
 
 void Chip8_InitializeAudio(Chip8_Audio *audio);
 void Chip8_PlayBeep(Chip8_Audio *audio);
