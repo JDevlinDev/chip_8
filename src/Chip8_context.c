2629c2aea742a911c9b81c6efd48508526d24c58
Jason Sherrick <jasonsherrick@gmail.com>
2026-07-21 19:03:26 -0500

    Handle CPU instruction cycles, delay, and sound timers

--- src/Chip8_context.c
+++ src/Chip8_context.c
@@ -1,5 +1,7 @@
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_init.h>
+#include <SDL3/SDL_timer.h>
+#include <SDL3/SDL_log.h>
 
 #include "Chip8_context.h"
 #include "Chip8_emulator.h"
@@ -25,17 +27,52 @@ bool Chip8_InitializeContext(Chip8_Context *ctx)
         return SDL_APP_FAILURE;
     }
     SDL_SetRenderLogicalPresentation(
-            &ctx->renderer,
+            ctx->renderer,
             CHIP8_SCREEN_WIDTH,
             CHIP8_SCREEN_HEIGHT,
             SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
    Chip8_Init(&ctx->emulator);
 
-   ctx->time.in_accumulator = 0;
-   ctx->time.tm_accumulator = 0;
+   ctx->time.i_accumulator = 0;
+   ctx->time.t_accumulator = 0;
    ctx->time.last = 0;
    ctx->is_running = false;
 }
 
-void Chip8_UpdateContext(Chip8_Context *ctx);
+void Chip8_UpdateContext(Chip8_Context *ctx)
+{
+   if (ctx->time.last == 0) {
+        ctx->time.last = SDL_GetTicksNS();
+    }
+    
+   uint64_t current_time = SDL_GetTicksNS();
+   uint64_t time_delta = current_time - ctx->time.last;
+   ctx->time.last = current_time;
+   
+   ctx->time.i_accumulator += time_delta;
+   ctx->time.t_accumulator += time_delta;
+
+   // Handle CPU instruction cycles
+   while (ctx->time.i_accumulator >= CHIP8_CLOCK_RATE_NS) {
+      if (!ctx->wait) {
+          uint16_t next_instruction = Chip8_Fetch(&ctx->emulator);
+          Chip8_Execute(&ctx->emulator, next_instruction);
+      }
+      ctx->time.i_accumulator -= CHIP8_CLOCK_RATE_NS;
+   }
+    
+   // Handle 60Hz Delay and Sound timers
+   while (ctx->time.t_accumulator >= CHIP8_TIMER_RATE_NS) {
+      if (ctx->emulator.registers.DT > 0) {
+         ctx->emulator.registers.DT--;
+      }
+   
+      if (ctx->emulator.registers.ST > 0) {
+         ctx->emulator.registers.ST--;
+         Chip8_PlayBeep(&ctx->emulator.audio);
+      }
+
+      ctx->time.t_accumulator -= CHIP8_TIMER_RATE_NS;
+   }
+}
