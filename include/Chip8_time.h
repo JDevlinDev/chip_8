9744022578229fcc0c8aabcaaaf88b4391f97893
Jason Sherrick <jasonsherrick@gmail.com>
2026-07-21 19:00:09 -0500

    Update property names

--- include/Chip8_time.h
+++ include/Chip8_time.h
@@ -6,7 +6,7 @@
 typedef Chip8_Emulator Chip8_Emulator;
 
 typedef struct Chip8_Time {
-   uint64_t    in_accumulator;
-   uint64_t    tm_accumulator;
+   uint64_t    i_accumulator;
+   uint64_t    t_accumulator;
    uint64_t    last;
 } Chip8_Time;
