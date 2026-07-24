7e6838d946fa5b4ff9784e32cf6815d718a0a88f
Jason Sherrick <jasonsherrick@gmail.com>
2026-07-23 21:00:06 -0500

    Remove static appState, add cycle limiter to reduce CPU usage

--- src/main.c
+++ src/main.c
@@ -13,11 +13,11 @@
 #include "Chip8_keyboard.h"
 #include "Chip8_appState.h"
 
-static Chip8_AppState app;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    Chip8_InitializeApp(&app);
+    Chip8_AppState *app = (Chip8_AppState *)SDL_calloc(1, sizeof(Chip8_AppState));
+    Chip8_InitializeApp(app);
 
     char *filename;
     if (argc > 1) {
@@ -27,36 +27,37 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         printf("File was not provided\n");
         exit(EXIT_FAILURE);
     }
-    size_t fsize = Chip8_Load(&app.emulator, filename);
+    size_t fsize = Chip8_Load(&app->emulator, filename);
 
     if (fsize == 0) {
         fprintf(stderr, "Failed to load file\n");
         exit(EXIT_FAILURE);
     }
-
+    *appstate = app;
     return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
+    Chip8_AppState *app = (Chip8_AppState *)appstate;
     int key;
 
     switch (event->type) {
         case SDL_EVENT_QUIT:
-            SDL_DestroyAudioStream(app.emulator.audio.stream);
+            
             return SDL_APP_SUCCESS;
 
         case SDL_EVENT_KEY_DOWN:
             key = Chip8_MapKey(event->key.scancode);
             if (key != -1) {
-                Chip8_KeyDown(&app.emulator.keyboard, key);
+                Chip8_KeyDown(&app->emulator.keyboard, key);
             }
             break;
         
         case SDL_EVENT_KEY_UP:
             key = Chip8_MapKey(event->key.scancode);
             if (key != -1) {
-                Chip8_KeyUp(&app.emulator.keyboard, key);
+                Chip8_KeyUp(&app->emulator.keyboard, key);
             }
             break;
     }
@@ -66,20 +67,38 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    Chip8_UpdateClock(&app);
+    Chip8_AppState *app = (Chip8_AppState *)appstate;
+
+    uint64_t loop_start = SDL_GetTicksNS();
+
+    Chip8_UpdateClock(app);
     
-    SDL_SetRenderDrawColor(app.renderer, 10, 10, 10, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(app.renderer);
+    SDL_SetRenderDrawColor(app->renderer, 10, 10, 10, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(app->renderer);
 
-    SDL_SetRenderDrawColor(app.renderer, 250, 250, 250, SDL_ALPHA_OPAQUE);
-    Chip8_RenderDisplay(&app.emulator.display, app.renderer);
+    SDL_SetRenderDrawColor(app->renderer, 250, 250, 250, SDL_ALPHA_OPAQUE);
+    Chip8_RenderDisplay(&app->emulator.display, app->renderer);
 
-    SDL_RenderPresent(app.renderer);
+    SDL_RenderPresent(app->renderer);
+
+    uint64_t loop_end = SDL_GetTicksNS();
+    uint64_t elapsed_ns = loop_end - loop_start;
+
+    // Limit the render loop to 700Hz to reduce CPU consumption
+    if (elapsed_ns < CHIP8_CPU_CLOCK_RATE_NS)
+        SDL_DelayNS(CHIP8_CPU_CLOCK_RATE_NS - elapsed_ns);
 
     return SDL_APP_CONTINUE;
 }
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
-    /* SDL will clean up the window/renderer for us. */
+
+    if (appstate) {
+        Chip8_AppState *as = (Chip8_AppState *)appstate;
+        if(as->emulator.audio.stream)
+            SDL_DestroyAudioStream(as->emulator.audio.stream);
+
+        SDL_free(as);
+    }
 }
