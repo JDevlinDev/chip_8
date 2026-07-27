967be78e66bcd7527c58f14f2d9e2a3a2498c9c3
Jason Sherrick <jasonsherrick@gmail.com>
2026-07-27 12:24:28 -0500

    Add prototype for dumping V registers to string

--- include/Chip8_registers.h
+++ include/Chip8_registers.h
@@ -15,3 +15,13 @@ typedef struct Chip8_Registers
     uint8_t DT;                    /* Delay Timer - Decrements at 60hz while > 0 */
     uint8_t ST;                    /* Sound Timer - Decrements at 60hz while > 0 */
 } Chip8_Registers;
+
+
+/**
+ * Dumps contents of V registers to a string. For printing to stdout or a log file.
+ * 
+ * \param[in] reg Struct containing the current register state
+ * \param[out] str A string to hold the V register contents
+ * \param[in] size Size of the output string
+ */
+void Chip8_VRegToString(const Chip8_Registers *reg, char *str, size_t size);
